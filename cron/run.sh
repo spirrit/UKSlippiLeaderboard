bcdfdfa19611b778319fe55b83a5a76d1662a2c9
spirrit <olivemoonlighter@gmail.com>
2023-01-06 22:38:29 +0000

    small fix

--- cron/run.sh
+++ cron/run.sh
@@ -1,4 +1,4 @@
 #!/bin/bash -l
 DIR_PATH=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
 cd "$DIR_PATH/.." 
-node.exe --loader ts-node/esm --no-warnings cron/fetchStats.ts 2>&1 | tee cron/logs/log.txt
+node --loader ts-node/esm --no-warnings cron/fetchStats.ts 2>&1 | tee cron/logs/log.txt
