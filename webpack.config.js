db09bf9e2ce4b958197f77b78a1ab8d9f76a74ca
spirrit <olivemoonlighter@gmail.com>
2023-01-07 13:32:52 +0000

    edit font style for slippi page

--- webpack.config.js
+++ webpack.config.js
@@ -91,6 +91,9 @@ module.exports = {
             <meta http-equiv="X-UA-Compatible" content="IE=edge">
             <meta name="viewport" content="width=device-width, initial-scale=1">
             <title>${settings.title}</title>
+            <link rel="preconnect" href="https://fonts.googleapis.com">
+            <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin="">
+            <link href="https://fonts.googleapis.com/css2?family=Quicksand&amp;display=swap" rel="stylesheet">
           </head>
           <body class="bg-gray-600">
             <noscript>
